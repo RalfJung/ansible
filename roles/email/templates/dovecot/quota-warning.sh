46730acf42cbde7d087b887eb78c3f06dc27d9ca
Ralf Jung <post@ralfj.de>
2018-05-20 18:58:07 +0200

    start dovecot config; generate postfix transport_map; support multiple sender IPs in postfix

---
+++ roles/email/templates/dovecot/quota-warning.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -e
+
+PERCENT=$1
+FROM="{{postfix.dovecot.postmaster}}"
+
+msg="From: $FROM
+To: $USER
+CC: $FROM
+Subject: Dein Posteingang ist $PERCENT% voll / Your mailbox is $PERCENT% full
+Content-Type: text/plain; charset=UTF-8
+
+Dein Posteingang ist zu $PERCENT% voll. Bitte räume etwas auf!
+
+Your mailbox is now $PERCENT% full. Please clean it up a bit!"
+
+echo -e "$msg" | /usr/sbin/sendmail -f "$FROM" "$USER" "$FROM"
+
+exit 0
